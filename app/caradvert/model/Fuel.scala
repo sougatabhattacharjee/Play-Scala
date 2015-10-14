d27c51159e96d3bae3de2975be46d259299f1ed6
sougatabhattacharjee <sougata.bhattacharjee@hotmail.com>
2015-10-14 08:50:14 +0200

    Fuel types added

---
+++ app/caradvert/model/Fuel.scala
@@ -0,0 +1,10 @@
+package caradvert.model
+
+/**
+ * Created by Sougata on 10/14/2015.
+ * fuel (required) has two types diesel or gasoline
+ */
+abstract class Fuel
+case object Diesel extends Fuel
+case object Gasoline extends Fuel
+

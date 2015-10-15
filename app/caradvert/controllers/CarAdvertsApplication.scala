aca25c927dadfc0141a3457aeb3e18f5c05b2b96
sougatabhattacharjee <sougata.bhattacharjee@hotmail.com>
2015-10-15 04:08:11 +0200

    Initial application entry point

--- app/caradvert/controllers/CarAdvertsApplication.scala
+++ app/caradvert/controllers/CarAdvertsApplication.scala
@@ -1,8 +1,17 @@
 package caradvert.controllers
 
+import caradvert.Persistence.CarAdvertsDaoImpl
+import com.google.inject.Inject
+import play.Logger
+import play.api.libs.json.{JsSuccess, JsError}
 import play.api.mvc._
+import play.libs.Json
+import caradvert.model.{CarAdvertsModelFormatter, CarAdvertsModel}
+import play.api.Play.current
 
-object CarAdvertsApplication extends Controller {
+
+class CarAdvertsApplication @Inject()(carAdvertsDaoImpl: CarAdvertsDaoImpl)
+                                     (implicit carAdvertsModelFormatter: CarAdvertsModelFormatter) extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
@@ -10,9 +19,17 @@ object CarAdvertsApplication extends Controller {
 
   def list = TODO
 
-  def list(id: Int) = TODO
+  def listone(id: Int) = TODO
 
-  def add = TODO
+  def add = Action(parse.json) { request =>
+    request.body.validate[CarAdvertsModel] match {
+      case success: JsSuccess[CarAdvertsModel] =>
+        Logger.info("One Car Added! " + success.get)
+        Created(Json.toJson(carAdvertsDaoImpl.addCar(success.get)))
+      case error: JsError =>
+        BadRequest(JsError.toJson(error))
+    }
+  }
 
   def modify(id: Int) = TODO
 

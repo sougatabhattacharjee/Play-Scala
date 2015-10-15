85aeea07681d632b3334632594b01de8433fb900
sougatabhattacharjee <sougata.bhattacharjee@hotmail.com>
2015-10-15 20:44:57 +0200

    dao Interface updated

--- app/caradvert/Persistence/CarAdvertsDaoImpl.scala
+++ app/caradvert/Persistence/CarAdvertsDaoImpl.scala
@@ -1,6 +1,6 @@
 package caradvert.Persistence
 
-import caradvert.model.{CarAdvertsUsed, CarAdvertsNew, CarAdvertsModel}
+import caradvert.model.{CarAdvertsModel, CarAdvertsNew, CarAdvertsUsed}
 import com.google.inject.ImplementedBy
 
 /**
@@ -10,11 +10,26 @@ import com.google.inject.ImplementedBy
 @ImplementedBy(classOf[CarAdvertsDao])
 trait CarAdvertsDaoImpl {
 
+  def list(): List[CarAdvertsModel] = list(None)
+  def list(sortBy: Option[String]): List[CarAdvertsModel] = list(sortBy.getOrElse("id"))
+  def list(sortBy: String): List[CarAdvertsModel]
+
+  def listOne(maybeId: Option[String]): Option[CarAdvertsModel] = maybeId match {
+    case Some(id) => listOne(id)
+    case None => None
+  }
+  def listOne(id: String): Option[CarAdvertsModel]
+
+
   def addCar(carAdvertsModel: CarAdvertsModel): CarAdvertsModel = carAdvertsModel match {
     case CarAdvertsNew(_,_,_,_,_) => addCar(carAdvertsModel.asInstanceOf[CarAdvertsNew])
     case CarAdvertsUsed(_,_,_,_,_,_,_) => addCar(carAdvertsModel.asInstanceOf[CarAdvertsUsed])
   }
   def addCar(carAdvertsNew: CarAdvertsNew): CarAdvertsModel
   def addCar(carAdvertsUsed: CarAdvertsUsed): CarAdvertsModel
+
+
+  def carDelete(id: String): Int
+
 
 }

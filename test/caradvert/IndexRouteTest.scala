d9d981f97324da9039ee7c942c47ab316a8a15ab
sougatabhattacharjee <sougata.bhattacharjee@hotmail.com>
2016-04-21 02:09:17 +0200

    updated

---
+++ test/caradvert/IndexRouteTest.scala
@@ -0,0 +1,12 @@
+package caradvert
+
+import org.specs2.Specification
+import org.specs2.specification.core.SpecStructure
+import play.api.mvc.Controller
+
+/**
+ * Created by Sougata on 10/16/2015.
+ */
+class IndexRouteTest extends Specification with Controller {
+  override def is: SpecStructure = ???
+}
